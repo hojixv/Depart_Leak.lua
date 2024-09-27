605aca255f4bcfc67f215190cae553c581e7c694
hojixv <153679337+hojixv@users.noreply.github.com>
2024-09-26 21:45:41 -0400

    Resources.lua

---
+++ Resources.lua
@@ -0,0 +1,21 @@
+local Inset = game:GetService("GuiService"):GetGuiInset().Y
+local Mouse = game.Players.LocalPlayer:GetMouse()
+local Client = game.Players.LocalPlayer
+local Cam = workspace.CurrentCamera
+local RunService = game:GetService("RunService")
+
+local RunService = game:GetService('RunService')
+local Players = game:GetService("Players")
+local Client = Players.LocalPlayer
+local Mouse = Client:GetMouse()
+local GuiS = game:GetService("GuiService")
+
+local runService = game:GetService("RunService")
+local userInputService = game:GetService("UserInputService")
+local playersService = game:GetService("Players")
+local workspace = game:GetService("Workspace")
+local camera = workspace.CurrentCamera
+local localPlayer = playersService.LocalPlayer
+local mouse = localPlayer:GetMouse()
+
+print 'Hi'
